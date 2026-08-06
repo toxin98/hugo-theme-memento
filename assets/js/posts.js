72d2602a3435257e6082e7eecb1ec74265e33dd8
toxin98 <839868794@qq.com>
2026-08-06 12:46:19 +0800

    posts year filter

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,27 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  document.querySelectorAll(".posts-category-title").forEach(button => {
+  const buttons = document.querySelectorAll(".posts-year-filter button");
+  const sections = document.querySelectorAll(".posts-section");
+
+  buttons.forEach(button => {
+
     button.addEventListener("click", () => {
 
-      document.querySelectorAll(".posts-category-title").forEach(b => {
-        b.classList.remove("active");
+      const year = button.dataset.filter;
+
+      buttons.forEach(btn => {
+        btn.classList.remove("active");
       });
+
       button.classList.add("active");
 
-      showYear(button.dataset.target);
+      sections.forEach(section => {
+
+        if (year === "all" || section.dataset.year === year) {
+          section.classList.remove("hidden");
+        } else {
+          section.classList.add("hidden");
+        }
+
+      });
+
     });
+
   });
-
-  showYear("all");
-
-  function showYear(year) {
-    document.querySelectorAll(".posts-section").forEach(el => {
-      if (year === "all") {
-        el.classList.add("show");
-      } else {
-        el.classList.toggle("show", el.classList.contains(year));
-      }
-    });
-  }
 
 });
